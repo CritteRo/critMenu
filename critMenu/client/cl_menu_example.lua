ab327a35e755157be1599e1a14c58d0d39603d49
CritteRo <critteras@gmail.com>
2021-06-20 01:37:27 +0300

    Update cl_menu_example.lua

--- critMenu/client/cl_menu_example.lua
+++ critMenu/client/cl_menu_example.lua
@@ -1,6 +1,6 @@
 
-TriggerEvent('critMenu.CreateMenu', "test1", "Test Menu", "Test Description", "Select", "Up", "Down", "Close Menu")
-TriggerEvent('critMenu.AddButton', "test1", "btn1", "Button 1", "", "test", "Descrption ", "test.event")
+TriggerEvent('critMenu.CreateMenu', "test1", "Test Menu", "Test Description", "~y~Select", "Up", "Down", "Close Menu")
+TriggerEvent('critMenu.AddButton', "test1", "btn1", "Button 1", "", "test", "~y~Descrp~b~tion~s~", "test.event")
 TriggerEvent('critMenu.AddButton', "test1", "btn2", "Button 2", ">>", "     ", "Descrption ", "test.event")
 TriggerEvent('critMenu.AddButton', "test1", "btn3", "Button 3", "", "", "Descrption ", "test.event")
 
