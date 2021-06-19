6b446af7bdc00174ad4822d5fb186841f82a4e54
CritteRo <critteras@gmail.com>
2021-06-19 21:19:02 +0300

    commit

---
+++ critMenu/client/cl_menu_handle.lua
@@ -0,0 +1,5 @@
+print('critMenu loaded.')
+
+RegisterCommand('front', function()
+    ActivateFrontendMenu("FE_MENU_VERSION_MP_PAUSE", true, 10)
+end)
---
+++ critMenu/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CritteR / CritteRo'
+description 'Some sort of menu, idk.'
+
+client_scripts {
+    'client/cl_menu_handle.lua',
+}
