6808e83f4803f9a071401d3331679651cd0dc227
CritteRo <critteras@gmail.com>
2021-06-21 20:59:33 +0300

    Update cl_menu_handle.lua

--- critMenu/client/cl_menu_handle.lua
+++ critMenu/client/cl_menu_handle.lua
@@ -66,7 +66,7 @@ AddEventHandler('critMenu.HideMenu', function()
     else
         local parent = menu[menuShown].menuParent
         if menu[parent] ~= nil then
-            --TriggerEvent('critMenu.Check.MenuWasClosed', menuShown)
+            TriggerEvent('critMenu.Check.MenuWasClosed', menuShown)
             generateMenu("critmenu:internalmenu:cleanmenu:donotuse", 1) --needed to reset the scene
             menuShown = parent
             instrucitonId = generateInstruction(menuShown)
