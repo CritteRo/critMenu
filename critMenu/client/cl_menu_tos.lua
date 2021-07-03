49ad1e709b0d79467270e99ad1640c0acdde92d9
CritteRo <critteras@gmail.com>
2021-07-03 23:08:12 +0300

    code cleanup

--- critMenu/client/cl_menu_tos.lua
+++ critMenu/client/cl_menu_tos.lua
@@ -1,14 +1,5 @@
 --[[ :: WORK IN PROGRESS :: ]]--
 
-tosId = nil
-
-tostitle = "Terms of Service and End-User Licence Agreement"
-tostext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis tellus quis nisl consectetur, eu vehicula risus scelerisque. Aliquam congue, nulla et mollis malesuada, justo risus sodales sapien, at viverra odio massa at dui. Nam orci massa, mollis ut leo nec, tristique congue sapien. Curabitur tempus accumsan magna, at elementum dui varius sit amet. Duis viverra ullamcorper sapien a vehicula. Vivamus gravida euismod ullamcorper. Morbi et mollis mauris, id facilisis quam. Proin at justo at lorem laoreet pellentesque. Vestibulum finibus urna quis orci tincidunt, in pretium arcu porttitor. In porttitor varius arcu at lobortis. Fusce in dictum arcu."
-tostext2 = "Quisque neque sapien, imperdiet at turpis eget, commodo pellentesque nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus sagittis erat ac justo auctor, id convallis odio luctus. Maecenas at lorem posuere, sodales felis at, varius erat. Ut pellentesque mi sem. Etiam blandit ut ipsum eu gravida. Pellentesque sodales quam odio, sed ultricies dolor consectetur ut. Vestibulum egestas ac libero consequat iaculis."
-tostext3 = "Nulla dignissim at nibh in egestas. Nunc congue lectus ac arcu tristique, placerat maximus orci cursus. Sed tempus pellentesque lectus, et rutrum mauris faucibus blandit. Vivamus nec vulputate tellus. Maecenas rutrum faucibus velit sed aliquam. Donec dui enim, venenatis non fringilla vel, ullamcorper eu elit. Maecenas dictum arcu non posuere hendrerit. Maecenas placerat faucibus turpis, vitae rhoncus erat fringilla vel. Sed id sapien quam. Pellentesque ornare pretium facilisis. Mauris volutpat odio malesuada lectus gravida efficitur."
-tostext4 = "In vel placerat enim. Donec at ornare massa. Nulla maximus mauris lorem, egestas luctus eros vulputate quis. Aenean imperdiet ipsum dolor, id maximus arcu ornare in. Nam id dignissim est. Nunc venenatis aliquet libero quis elementum. Proin malesuada fermentum mauris eget ultrices. Phasellus elementum lorem nisl, et lacinia ex porttitor in. Ut fringilla tristique aliquam. Maecenas porttitor nibh ante, eget luctus leo ullamcorper sed."
-
-
 function generateTos(menuID)
     local scaleform = RequestScaleformMovie("ONLINE_POLICIES")
     while not HasScaleformMovieLoaded(scaleform) do
